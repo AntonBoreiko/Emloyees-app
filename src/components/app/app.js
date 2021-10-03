e6f65670e4ab5cb5e0ed8c28b6f55ea9865ad295
AntonBoreiko <1213.96@mail.ru>
2021-10-03 20:44:59 +0300

    Fixed counter of emloyees

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -44,20 +44,17 @@ class App extends Component {
       })
    }
 
-   onToggleIncrease = (id) => {
+   onToggleProp = (id, prop) => {
       this.setState(({ data }) => ({
          data: data.map(item => {
             if (item.id === id) {
-               return { ...item, increase: !item.increase }
+               return { ...item, [prop]: !item[prop] }
             }
             return item
          })
       }))
    }
 
-   onToggleRise = (id) => {
-      console.log(`Rise this ${id}`)
-   }
 
    render() {
       const employees = this.state.data.length
@@ -69,7 +66,7 @@ class App extends Component {
                employees={employees}
                increased={increased} />
 
-            
+
             <div className="search-panel">
                <SearchPanel />
                <AppFilter />
@@ -77,8 +74,8 @@ class App extends Component {
             <EmployeesList
                data={this.state.data}
                onDelete={this.deleteItem}
-               onToggleIncrease={this.onToggleIncrease}
-               onToggleRise={this.onToggleRise}
+               onToggleProp={this.onToggleProp}
+
             />
             <EmployeesAddForm onAdd={this.addItem} />
          </div>
