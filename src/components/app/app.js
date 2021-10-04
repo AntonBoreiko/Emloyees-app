c6dc18f037d74b33813392a08ed9247422f2cf5e
AntonBoreiko <1213.96@mail.ru>
2021-10-04 23:44:10 +0300

    Add employees filter

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -18,7 +18,8 @@ class App extends Component {
             { name: "Mark V.", salary: 3000, increase: false, rise: false, id: 2 },
             { name: "Pavel K.", salary: 4000, increase: true, rise: false, id: 3 },
          ],
-         term: ''
+         term: '',
+         filter: 'all'
       }
       this.maxId = 4
    }
@@ -46,6 +47,7 @@ class App extends Component {
       })
    }
 
+
    onToggleProp = (id, prop) => {
       this.setState(({ data }) => ({
          data: data.map(item => {
@@ -71,12 +73,26 @@ class App extends Component {
       this.setState({ term })
    }
 
+   filterPost = (items, filter) => {
+      switch (filter) {
+         case 'rise':
+            return items.filter(item => item.rise)
+         case 'moreThen1000':
+            return items.filter(item => item.salary > 1000)
+         default:
+            return items
+      }
+   }
+
+   onFilterSelect = (filter) => {
+      this.setState({ filter })
+   }
    render() {
-      const { data, term } = this.state
+      const { data, term, filter } = this.state
       const employees = this.state.data.length
       const increased = this.state.data.filter(item => item.increase).length
 
-      const visibleData = this.searchEmp(data, term)
+      const visibleData = this.filterPost(this.searchEmp(data, term), filter)
       return (
          <div className="app">
             <AppInfo
@@ -86,7 +102,9 @@ class App extends Component {
 
             <div className="search-panel">
                <SearchPanel onUpdateSearch={this.onUpdateSearch} />
-               <AppFilter />
+               <AppFilter filter={filter}
+                  onFilterSelect={this.onFilterSelect}
+               />
             </div>
             <EmployeesList
                data={visibleData}
